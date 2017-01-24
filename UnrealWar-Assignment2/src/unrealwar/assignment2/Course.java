acfd3992381d59c3cec3e6287745a54f17628ebb
chads <chads@192.168.1.11>
2017-01-24 13:05:33 -0500

    Added Map method. Fixed remove + get to avoid collection issue.Added null to position method

--- UnrealWar-Assignment2/src/unrealwar/assignment2/Course.java
+++ UnrealWar-Assignment2/src/unrealwar/assignment2/Course.java
@@ -11,9 +11,11 @@ import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
-import java.util.TreeMap;
+
 /**
  *
  * @author c0533886
@@ -41,40 +43,44 @@ public class Course {
     }
 
     public void remove(String id) {
-        for(Student s : students) {
-            if(s.getId().equals(id)) {
-                students.remove(id);
+        int i;
+        for (i = 0; i < students.size(); i++) {
+            if (students.get(i).getId().equals(id)) {
+                students.remove(i);
             }
         }
-        
+
     }
 
     public void remove(int position) {
         students.remove(position);
     }
 
-
     public void insert(Student s, int position) {
         students.set(position, s);
     }
 
     public Student get(String id) {
-        for (Student s : students) {
-            if (s.getId().equals(id)) {
-                return s;
+        int i;
+        for (i = 0; i < students.size(); i++) {
+            if (students.get(i).getId().equals(id)) {
+                return students.get(i);
             }
         }
         return null;
     }
 
     public Student get(int position) {
+        if (position >= students.size() || position < 0) {
+            return null;
+        }
         return students.get(position);
     }
 
     public List<Student> getAll() {
-         
-      return students;
-      
+
+        return students;
+
     }
 
     @Override
@@ -99,16 +105,43 @@ public class Course {
     }
 
     public Student getAllbyGender(String gender) {
-        for (Student s : students){
-            if (s.getGender().equals(gender)){
+        for (Student s : students) {
+            if (s.getGender().equals(gender)) {
                 return s;
             }
         }
         return null;
     }
 
-    public void getGradeMap() {
-//        Map<String,Set<Student>> students = new TreeMap<>();
-//        return ;
+    public Map<String, Set<Student>> getGradeMap() {
+        Map<String, Set<Student>> s = new HashMap<String, Set<Student>>();
+
+        Set<Student> studentA = new HashSet<Student>();
+        Set<Student> studentB = new HashSet<Student>();
+        Set<Student> studentC = new HashSet<Student>();
+        Set<Student> studentD = new HashSet<Student>();
+        Set<Student> studentE = new HashSet<Student>();
+
+        for (int x = 0; x < students.size(); x++) {
+            if (students.get(x).getGrade() < 1) {
+                studentA.add(students.get(x));
+            } else if (students.get(x).getGrade() >= 1 && students.get(x).getGrade() < 2) {
+                studentB.add(students.get(x));
+            } else if (students.get(x).getGrade() >= 2 && students.get(x).getGrade() < 3) {
+                studentC.add(students.get(x));
+            } else if (students.get(x).getGrade() >= 3 && students.get(x).getGrade() < 4) {
+                studentD.add(students.get(x));
+            } else if (students.get(x).getGrade() >= 4 && students.get(x).getGrade() < 5) {
+                studentE.add(students.get(x));
+            }
+        }
+
+        s.put("A", studentE);
+        s.put("B", studentD);
+        s.put("C", studentC);
+        s.put("D", studentB);
+        s.put("F", studentA);
+
+        return s;
     }
 }
