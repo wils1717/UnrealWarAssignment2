fec6e3b4a3ea45ebf9ad80ab17561b7109c93a1c
c0687799 <c0687799@SB-N208-32.lambton.on.ca>
2017-01-25 11:24:34 -0500

    Test of Course Class with List parameter

--- UnrealWar-Assignment2/test/unrealwar/assignment2/CourseTest.java
+++ UnrealWar-Assignment2/test/unrealwar/assignment2/CourseTest.java
@@ -5,6 +5,8 @@
  */
 package unrealwar.assignment2;
 
+import java.util.ArrayList;
+import java.util.List;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
@@ -20,136 +22,6 @@ import static org.junit.Assert.*;
  * @author c0121833
  */
 public class CourseTest {
-
-    public CourseTest() {
-    }
-
-    @BeforeClass
-    public static void setUpClass() {
-    }
-
-    @AfterClass
-    public static void tearDownClass() {
-    }
-
-    @Before
-    public void setUp() {
-    }
-
-    @After
-    public void tearDown() {
-    }
-
-    /**
-     * Test of add method, of class Course.
-     */
-    @Test
-    public void testAdd() {
-        System.out.println("add");
-        Student s = null;
-        Course instance = new Course();
-        instance.add(s);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of remove method, of class Course.
-     */
-    @Test
-    public void testRemove_Student() {
-        System.out.println("remove");
-        Student student = null;
-        Course instance = new Course();
-        instance.remove(student);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of remove method, of class Course.
-     */
-    @Test
-    public void testRemove_String() {
-        System.out.println("remove");
-        String id = "";
-        Course instance = new Course();
-        instance.remove(id);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of remove method, of class Course.
-     */
-    @Test
-    public void testRemove_int() {
-        System.out.println("remove");
-        int position = 0;
-        Course instance = new Course();
-        instance.remove(position);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of insert method, of class Course.
-     */
-    @Test
-    public void testInsert() {
-        System.out.println("insert");
-        Student student = null;
-        int position = 0;
-        Course instance = new Course();
-        instance.insert(student, position);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of get method, of class Course.
-     */
-    @Test
-    public void testGet_String() {
-        System.out.println("get");
-        String id = "";
-        Course instance = new Course();
-        Student expResult = null;
-        Student result = instance.get(id);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of get method, of class Course.
-     */
-    @Test
-    public void testGet_int() {
-        System.out.println("get");
-        int position = 0;
-        Course instance = new Course();
-        Student expResult = null;
-        Student result = instance.get(position);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of equals method, of class Course.
-     */
-    @Test
-    public void testEquals() {
-        System.out.println("equals");
-        Object obj = null;
-        Course instance = new Course();
-        boolean expResult = false;
-        boolean result = instance.equals(obj);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
 
     /**
      * Test of toString method, of class Course.
@@ -187,29 +59,26 @@ public class CourseTest {
         }
     }
 
-    /**
-     * Test of getAllbyGender method, of class Course.
-     */
     @Test
-    public void testGetAllbyGender() {
-        System.out.println("getAllbyGender");
-        String gender = "";
-        Course instance = new Course();
-        instance.getAllbyGender(gender);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+    public void testListParameter() {  
+        System.out.println("Course(...)");
+        List students = new ArrayList();
+        students.add("s");
+        Course instance = new Course(students);         
+        assertEquals(students, instance.getAll());
     }
+    
+    /*@Test
+    public void testAddStudentParameter() {
+       System.out.println("Course(...)");
+        Student students = null;
+        
+        students.add("s");
+        Course instance = new Course();    
 
-    /**
-     * Test of getGradeMap method, of class Course.
-     */
-    @Test
-    public void testGetGradeMap() {
-        System.out.println("getGradeMap");
-        Course instance = new Course();
-        instance.getGradeMap();
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
+        assertEquals(instance, instance.getAll());
+    }*/
+    
+    
+    
 }
