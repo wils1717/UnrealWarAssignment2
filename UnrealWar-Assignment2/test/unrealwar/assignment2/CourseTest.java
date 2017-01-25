ad12bfb8026ad8b06c08c17b562f814a770a15c7
c0687799 <c0687799@SB-N208-32.lambton.on.ca>
2017-01-25 13:22:54 -0500

    Added two tests for CourseTest

--- UnrealWar-Assignment2/test/unrealwar/assignment2/CourseTest.java
+++ UnrealWar-Assignment2/test/unrealwar/assignment2/CourseTest.java
@@ -7,6 +7,7 @@ package unrealwar.assignment2;
 
 import java.util.ArrayList;
 import java.util.List;
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
@@ -23,6 +24,26 @@ import static org.junit.Assert.*;
  */
 public class CourseTest {
 
+    List<Student> studentList;
+    
+    public CourseTest(){
+    }
+    
+    @BeforeClass
+    public static void setUpClass(){
+    }
+
+    @AfterClass
+    public static void tearDownClass(){
+    }
+    
+    @Before
+    public void setUp(){
+        studentList = new ArrayList<>();
+        studentList.add(new Student("Bob", "ABC123", "Male", 65));
+        studentList.add(new Student("Jim", "ABC124", "Male", 60));
+               
+    }
     /**
      * Test of toString method, of class Course.
      */
@@ -37,48 +58,63 @@ public class CourseTest {
             instance1.setGrade(89.3);
             Student instance2 = new Student();
             instance2.setName("Suzie Chen");
-            instance2.setId("c0123467");
+            instance2.setId("c0123567");
             instance2.setGender("female");
             instance2.setGrade(92.4);
             Student instance3 = new Student();
-            instance2.setName("Ranbir Singh");
-            instance2.setId("c0123890");
-            instance2.setGender("male");
-            instance2.setGrade(87.1);
-            String jsonString1 = instance1.toString();
-            String jsonString2 = instance2.toString();
-            String jsonString3 = instance3.toString();
-            JSONObject result = (JSONObject) new JSONParser().parse(jsonString1 + jsonString2 + jsonString3);
-            JSONObject expResult = (JSONObject) new JSONParser().parse("{ \"name\" : \"Bill Smith\", \"id\" : \"c0123456\", \"gender\" : \"male\", \"grade\" : 89.3 }"
-                    +     "{ \"name\" : \"Suzie Chen\", \"id\" : \"c0123567\", \"gender\" : \"female\", \"grade\" : 92.4 }"
-                    +     "{ \"name\" : \"Ranbir Singh\", \"id\" : \"c0123890\", \"gender\" : \"male\", \"grade\" : 87.1 }");
+            instance3.setName("Ranbir Singh");
+            instance3.setId("c0123890");
+            instance3.setGender("male");
+            instance3.setGrade(87.1);
+            Course c = new Course();
+            c.add (instance1);
+            c.add (instance2);
+            c.add (instance3);
+            JSONArray result = (JSONArray) new JSONParser().parse(c.toString());
+            JSONArray expResult = (JSONArray) new JSONParser().parse("[{ \"name\" : \"Bill Smith\", \"id\" : \"c0123456\", \"gender\" : \"male\", \"grade\" : 89.3 },\n"
+                    +     "{ \"name\" : \"Suzie Chen\", \"id\" : \"c0123567\", \"gender\" : \"female\", \"grade\" : 92.4 },\n"
+                    +     "{ \"name\" : \"Ranbir Singh\", \"id\" : \"c0123890\", \"gender\" : \"male\", \"grade\" : 87.1 }]");
             assertEquals(expResult, result);
         } catch (ParseException ex) {
             System.err.println("Invalid JSON Format");
             fail("Invalid JSON Format");
         }
     }
-
+    
+    
+////    @Test
+//    public void testCourseConstructorWithListParamater() {
+//        Course instance = new Course(studentList);
+//        Student result = ;
+//        Student expResult = new Student();
+//        assertEquals(expResult, result);
+//    }
+    
+    /*Test for 
+    After calling the Course constructor with a List parameter, and using get() 
+    with a given id String of a valid student, a Student object with the correct 
+    id, name, gender and grade should be returned.*/ 
+    
     @Test
-    public void testListParameter() {  
-        System.out.println("Course(...)");
-        List students = new ArrayList();
-        students.add("s");
-        Course instance = new Course(students);         
-        assertEquals(students, instance.getAll());
+    public void testCanGetStudentFromList() {
+        Course instance = new Course(studentList);
+        Student result = instance.get("ABC555");
+        Student expResult = new Student("Bob", "ABC555", "male", 60);
+        assertEquals(expResult, result);
     }
     
-    /*@Test
-    public void testAddStudentParameter() {
-       System.out.println("Course(...)");
-        Student students = null;
-        
-        students.add("s");
-        Course instance = new Course();    
-
-        assertEquals(instance, instance.getAll());
-    }*/
+    /* Test for
+    After calling the Course constructor with a List parameter, and using get() 
+    with a given id String of an invalid student, a null pointer should be 
+    returned.*/   
     
+    @Test
+    public void testInvalidStudentId() {
+        Course instance = new Course(studentList);
+        Student result = instance.get("ABC555");
+        Student expResult = null;
+        assertEquals(expResult, result);
+    }
     
     
 }
